5954da3528bb7bc94bfc42b7df4c40cba3bfb91b
Alex05061995 <98837973+Alex05061995@users.noreply.github.com>
2023-01-18 11:05:02 +0300

    Update chooseCard.js

--- Task-2/js/chooseCard.js
+++ Task-2/js/chooseCard.js
@@ -1,6 +1,7 @@
 export function chooseCard(e) {
     const circle = e.currentTarget.querySelector('.product__weight');
     const phrase = e.currentTarget.querySelector('.product__phrase');
+    const border = e.currentTarget.querySelector('.product__border');
 
     if (e.currentTarget.dataset.available === 'false') {
         return
@@ -9,14 +10,20 @@ export function chooseCard(e) {
     if (e.currentTarget.dataset.taken === 'false') {
         console.log('dfdsfdsd');
         e.currentTarget.dataset.taken = 'true';
-        e.currentTarget.style.border = '3px solid #D91667'
+        e.currentTarget.classList.remove('border-none')
+        e.currentTarget.classList.add('border')
         circle.style.backgroundColor = '#D91667';
         phrase.innerText = '';
         phrase.innerText = 'Печень утки разварная с артишоками.'
+        border.classList.remove('display-none');
+        border.classList.add('display-block');
     } else {
         e.currentTarget.dataset.taken = 'false';
-        e.currentTarget.style.border = 'none';
+        e.currentTarget.classList.remove('border')
+        e.currentTarget.classList.add('border-none');
         circle.style.backgroundColor = '#1698D9';
-        phrase.innerHTML = 'Чего сидишь? Порадуй котэ, <a class="product__buy" href="#">купи</a> .'
+        phrase.innerHTML = 'Чего сидишь? Порадуй котэ, <a class="product__buy" href="#">купи</a> .';
+        border.classList.add('display-none');
+        border.classList.remove('display-block');
     }
 }
